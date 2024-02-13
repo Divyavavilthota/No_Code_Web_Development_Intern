e28407f519548bd7b24726f84964305cb51be7ff
Divya Vavilthota <99669084+Divyavavilthota@users.noreply.github.com>
2024-02-13 06:58:16 +0530

    Add files via upload

---
+++ intern/example.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("formContainer").style.display = "block";
+    }, 10000); // Show form after 15 seconds
+});
